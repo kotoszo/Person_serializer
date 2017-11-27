a174d9cd77ce9317060946d38319d33dcbea84e0
Unknown <rtomilobil@gmail.com>
2017-11-27 10:12:32 +0100

    init person class

---
+++ Logic/Person/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    [Serializable]
+    public class Person
+    {
+        private string name, address, phoneNumber;
+        public string Name { get => name; set => name = value; }
+        public string Address { get => address; set => address = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+
+        private DateTime recordDate;
+        public DateTime RecordDate { get => recordDate; set => recordDate = value; }
+
+        [NonSerialized] private int serialNumber;
+        public int SerialNumber { get => serialNumber; set => serialNumber = value; }
+
+        public Person()
+        {
+
+        }
+    }
+}
