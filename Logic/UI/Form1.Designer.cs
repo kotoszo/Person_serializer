5e0a7af664ad69c1bf847ca66ffbc2965d310c97
Unknown <rtomilobil@gmail.com>
2017-11-27 15:13:16 +0100

    add new Button and eventHandlers

--- Logic/UI/Form1.Designer.cs
+++ Logic/UI/Form1.Designer.cs
@@ -28,108 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.addressBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.phoneBox = new System.Windows.Forms.TextBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.firstButton = new System.Windows.Forms.Button();
+            this.lastButton = new System.Windows.Forms.Button();
+            this.helpLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // previousButton
             // 
-            this.button1.Location = new System.Drawing.Point(57, 369);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.previousButton.Location = new System.Drawing.Point(57, 369);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(75, 23);
+            this.previousButton.TabIndex = 0;
+            this.previousButton.Text = "Previous";
+            this.previousButton.UseVisualStyleBackColor = true;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
             // 
-            // textBox1
+            // nameBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(57, 83);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(353, 22);
-            this.textBox1.TabIndex = 1;
+            this.nameBox.Location = new System.Drawing.Point(57, 83);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(353, 22);
+            this.nameBox.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(54, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(45, 17);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(54, 157);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.Size = new System.Drawing.Size(60, 17);
             this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.label2.Text = "Address";
             // 
-            // textBox2
+            // addressBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(57, 206);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(353, 22);
-            this.textBox2.TabIndex = 3;
+            this.addressBox.Location = new System.Drawing.Point(57, 206);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(353, 22);
+            this.addressBox.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(54, 269);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.Size = new System.Drawing.Size(49, 17);
             this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
+            this.label3.Text = "Phone";
             // 
-            // textBox3
+            // phoneBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(57, 318);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(353, 22);
-            this.textBox3.TabIndex = 5;
+            this.phoneBox.Location = new System.Drawing.Point(57, 318);
+            this.phoneBox.Name = "phoneBox";
+            this.phoneBox.Size = new System.Drawing.Size(353, 22);
+            this.phoneBox.TabIndex = 5;
             // 
-            // button2
+            // nextButton
             // 
-            this.button2.Location = new System.Drawing.Point(199, 369);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.nextButton.Location = new System.Drawing.Point(199, 369);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 23);
+            this.nextButton.TabIndex = 7;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
             // 
-            // button3
+            // saveButton
             // 
-            this.button3.Location = new System.Drawing.Point(335, 369);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.saveButton.Location = new System.Drawing.Point(335, 369);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // firstButton
+            // 
+            this.firstButton.Location = new System.Drawing.Point(481, 82);
+            this.firstButton.Name = "firstButton";
+            this.firstButton.Size = new System.Drawing.Size(75, 23);
+            this.firstButton.TabIndex = 9;
+            this.firstButton.Text = "First";
+            this.firstButton.UseVisualStyleBackColor = true;
+            this.firstButton.Click += new System.EventHandler(this.first_Click);
+            // 
+            // lastButton
+            // 
+            this.lastButton.Location = new System.Drawing.Point(647, 83);
+            this.lastButton.Name = "lastButton";
+            this.lastButton.Size = new System.Drawing.Size(75, 23);
+            this.lastButton.TabIndex = 10;
+            this.lastButton.Text = "Last";
+            this.lastButton.UseVisualStyleBackColor = true;
+            this.lastButton.Click += new System.EventHandler(this.last_Click);
+            // 
+            // helpLabel
+            // 
+            this.helpLabel.AutoSize = true;
+            this.helpLabel.Location = new System.Drawing.Point(478, 157);
+            this.helpLabel.Name = "helpLabel";
+            this.helpLabel.Size = new System.Drawing.Size(95, 17);
+            this.helpLabel.TabIndex = 11;
+            this.helpLabel.Text = "Click For Help";
+            this.helpLabel.Click += new System.EventHandler(this.label4_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(768, 417);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.helpLabel);
+            this.Controls.Add(this.lastButton);
+            this.Controls.Add(this.firstButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.nextButton);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.phoneBox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.addressBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.previousButton);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Person";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,15 +177,18 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.TextBox nameBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox addressBox;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox phoneBox;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button firstButton;
+        private System.Windows.Forms.Button lastButton;
+        private System.Windows.Forms.Label helpLabel;
     }
 }
 
--- Logic/UI/Form1.cs
+++ Logic/UI/Form1.cs
@@ -7,14 +7,106 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Logic;
 
 namespace UI
 {
     public partial class Form1 : Form
     {
+
+        private Person person;
+        private int index;
+        private bool isSaved, isChange, isClicked;
+
         public Form1()
         {
             InitializeComponent();
+            isSaved = false;
+            isClicked = false;
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            index = 1;
+            FormFill();
+        }
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            isSaved = false;
+            index--;
+            if (index == 0) { index = 1; }
+            FormFill();
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if (IsFilled())
+            {
+                isSaved = false;
+                index++;
+                if (index == 100) { index = 99; }
+                FormFill();
+            }
+        }
+
+        private bool IsFilled()
+        {
+            return nameBox.Text.Length > 0 && phoneBox.Text.Length > 0 && addressBox.Text.Length > 0;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Validator validator = new Validator();
+            if (validator.Helper(nameBox.Text, addressBox.Text, phoneBox.Text))
+            {
+                person.Name = nameBox.Text;
+                person.Address = addressBox.Text;
+                person.PhoneNumber = phoneBox.Text;
+                person.SerialNumber = index;
+                person.Serialize();
+                isSaved = true;
+            } else
+            {
+                var msg = MessageBox.Show(validator.ToString(), "Something went wrong....");
+            }
+        }
+
+        private void first_Click(object sender, EventArgs e)
+        {
+            index = 1;
+            FormFill();
+        }
+
+        private void last_Click(object sender, EventArgs e)
+        {
+            index = Person.GetLastIndex();
+            FormFill();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            isClicked = !isClicked;
+            if (isClicked)
+            {
+                string header = "Solution: \n";
+                string address = "\nAddress:\nPostalCode City Street House number\n";
+                string phone = "\nPhone:\n1234-1234-123";
+                helpLabel.Text = header + address + phone;
+            } else
+            {
+                helpLabel.Text = "Click For Help";
+            }
+        }
+
+        private void FormFill()
+        {
+            person = Person.Deserialize(index);
+            nameBox.Text = person.Name;
+            addressBox.Text = person.Address;
+            phoneBox.Text = person.PhoneNumber;
+        }
+        
+
     }
 }
